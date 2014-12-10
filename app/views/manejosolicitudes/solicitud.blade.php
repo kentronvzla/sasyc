a95cbfa9dad0b60adc568dc3333e14364030ad2e
Richard Arrieta <Richard Arrieta@LENOVO-MJ83W43.kentron.com.ve>
2014-12-10 12:56:22 -0430

    Integración de solicitud

--- app/views/manejosolicitudes/solicitud.blade.php
+++ app/views/manejosolicitudes/solicitud.blade.php
@@ -1,116 +1,106 @@
-@extends('layouts.master')
-@section('contenido')
-<div class="panel panel-primary">
-    <div class="panel-heading"><strong>Datos de Solicitud</strong></div>  
-    <div class="panel-body">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <strong>Numero de Solicitud:12575</strong> 
-        </div>
-        <br><br>
-         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Descripción del caso">
-            </div>
-        </div>
-         <div class="col-xs-12 col-sm-12 col-md-5">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Referido por">
-            </div>
-        </div> 
-
-        <div class="col-xs-12 col-sm-12 col-md-3">
-            <div class="form-group">
-                <strong> Accion Inmediata</strong>
-                <div class="btn-group" data-toggle="buttons">
-                    <label class="btn btn-primary">
-                        Si
-                    </label>
-                    <label class="btn btn-default">
-                        No
-                    </label>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Actividad">
-            </div>
-        </div> 
-         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Referencia">
-            </div>
-        </div>
-
-         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Accion tomada">
-            </div>
-        </div> 
-
-         <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="form-group">
-                <select class="form-control" placeholder="Recepcion">
-                    <option value="" selected="selected">Recepcion</option>
-                    <option value="">Presidencia</option>
-                    <option value="">Correspondencia</option>
-                    <option value="">Ayudantia</option>
-                    <option value="">Gobernacion</option>
-                </select> 
-            </div>
-        </div>
-         <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="form-group">
-                <select class="form-control" placeholder="Procesado por">
-                    <option value="" selected="selected">Procesado por</option>
-                    <option value="" >FPS</option>
-                    <option value="" >IVSS</option>
-                    <option value="" >Barrio Adentro</option>
-                    <option value="" >Mision Vivienda</option>
-                    <option value="" >Otros</option>
-
-                </select> 
-            </div>
-        </div>
-       <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="form-group">
-                <select class="form-control" placeholder="Tipo de Solicitud">
-                    <option value="" selected="selected">Tipo de Solicitud</option>
-                    <option value="">Salud</option>
-                    <option value="">Economico</option>
-                </select> 
-            </div>
-        </div>
-         <div class="col-xs-12 col-sm-12 col-md-6">
-            <div class="form-group">
-                <select class="form-control" placeholder="Area">
-                    <option value="" >Area</option>
-                    <option value="" >Cirugia</option>
-                    <option value="" >Ginecologico</option>
-                    <option value="" >Nefrologia</option>
-                    <option value="" >Obstetricia</option>
-                    <option value="" >Odontologia</option>
-                    <option value="" >Traumatologia</option>
-                </select> 
-            </div>
-        </div>
-       
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Nececidad">
-            </div>
-        </div>
-      <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Observaciones">
-            </div>
-        </div>  
+<div class="panel-body">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <strong>Numero de Solicitud:12575</strong> 
     </div>
+    <br><br>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Descripción del caso">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-5">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Referido por">
+        </div>
+    </div> 
+
+    <div class="col-xs-12 col-sm-12 col-md-3">
+        <div class="form-group">
+            <strong> Accion Inmediata</strong>
+            <div class="btn-group" data-toggle="buttons">
+                <label class="btn btn-primary">
+                    Si
+                </label>
+                <label class="btn btn-default">
+                    No
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Actividad">
+        </div>
+    </div> 
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Referencia">
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Accion tomada">
+        </div>
+    </div> 
+
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="form-group">
+            <select class="form-control" placeholder="Recepcion">
+                <option value="" selected="selected">Recepcion</option>
+                <option value="">Presidencia</option>
+                <option value="">Correspondencia</option>
+                <option value="">Ayudantia</option>
+                <option value="">Gobernacion</option>
+            </select> 
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="form-group">
+            <select class="form-control" placeholder="Procesado por">
+                <option value="" selected="selected">Procesado por</option>
+                <option value="" >FPS</option>
+                <option value="" >IVSS</option>
+                <option value="" >Barrio Adentro</option>
+                <option value="" >Mision Vivienda</option>
+                <option value="" >Otros</option>
+
+            </select> 
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="form-group">
+            <select class="form-control" placeholder="Tipo de Solicitud">
+                <option value="" selected="selected">Tipo de Solicitud</option>
+                <option value="">Salud</option>
+                <option value="">Economico</option>
+            </select> 
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6">
+        <div class="form-group">
+            <select class="form-control" placeholder="Area">
+                <option value="" >Area</option>
+                <option value="" >Cirugia</option>
+                <option value="" >Ginecologico</option>
+                <option value="" >Nefrologia</option>
+                <option value="" >Obstetricia</option>
+                <option value="" >Odontologia</option>
+                <option value="" >Traumatologia</option>
+            </select> 
+        </div>
+    </div>
+
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Nececidad">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Observaciones">
+        </div>
+    </div>  
 </div>
-
-
-
-
-@stop
