f2022009ed2aea3a94dfc48ff1be1673e9f376be
Nadin arturo <arasmit_yamaui@hotmail.com>
2015-02-27 15:07:49 -0430

    Agregar usuarios

--- app/controllers/administracion/seguridad/UsuariosController.php
+++ app/controllers/administracion/seguridad/UsuariosController.php
@@ -1,16 +1,26 @@
 <?php namespace Administracion\Seguridad;
 
+use Illuminate\Support\Facades\Input;
+
 class UsuariosController extends \Administracion\TablasBaseController {
 
     public function __construct() {
         parent::__construct();
     }
 
-    public function afterPostIndex($usuario){
-        $usuario->grupos()->sync(\Input::get('grupos', []));
+    public function postIndex() {
+        $usuario = \Usuario::findOrNew(Input::get('id'));
+        $usuario->fill(\Input::all());
         if(\Input::get('password')!=""){
             $usuario->password = \Input::get('password');
         }
-        $usuario->save();
+        if ($usuario->save()) {
+            $usuario->grupos()->sync(\Input::get('grupos', []));
+            return \Redirect::to('administracion/seguridad/usuarios')
+                ->with('mensaje', 'Se guardo el usuario correctamente.');
+        } else {
+            return \Redirect::back()->withInput()
+                ->withErrors($usuario->getErrors());
+        }
     }
 }
