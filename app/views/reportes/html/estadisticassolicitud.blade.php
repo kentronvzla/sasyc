38446661a8d965d3ae71ad1e9d460eb7e2431056
Nadin arturo <arasmit_yamaui@hotmail.com>
2015-03-17 16:25:31 -0430

    Correcion en el reporte de agrupacion

--- app/views/reportes/html/estadisticassolicitud.blade.php
+++ app/views/reportes/html/estadisticassolicitud.blade.php
@@ -12,45 +12,47 @@
                 <th style="width: 10%;" class="titulo-tabla texto-derecha">Monto</th>
             </tr>
             </thead>
-            <?php $anterior = $solicitudes[$i]->first()->{$primera_columna[$i]}; ?>
-            @foreach($solicitudes[$i] as $solicitud)
-                @if(count($columnas[$i])>1 && $anterior!=$solicitud->{$primera_columna[$i]})
+            @if(count($solicitudes[$i]))
+                <?php $anterior = $solicitudes[$i]->first()->{$primera_columna[$i]}; ?>
+                @foreach($solicitudes[$i] as $solicitud)
+                    @if(count($columnas[$i])>1 && $anterior!=$solicitud->{$primera_columna[$i]})
+                        <tr>
+                            <th class="titulo-tabla" colspan="{{count($columnas[$i])}}">Total {{ $solicitud->getValorReporte($primera_columna[$i]) }}</th>
+                            <th class="titulo-tabla texto-derecha">{{$cont}}</th>
+                            <th class="titulo-tabla texto-derecha">{{tm($acum)}}</th>
+                        </tr>
+                        <?php $cont = 0; $acum=0; ?>
+                    @endif
+                    <tr>
+                        @foreach($columnas[$i] as $key=>$columna)
+                            <td style="width: {{80/count($columnas[$i])}}%;" class="fila-tabla">{{ $solicitud->getValorReporte($key) }}</td>
+                        @endforeach
+                        <td class="fila-tabla texto-derecha" style="width: 10%;">
+                            {{$solicitud->cantidad}}
+                        </td>
+                        <td class="fila-tabla texto-derecha" style="width: 10%;">
+                            {{tm($solicitud->monto)}}
+                        </td>
+                    </tr>
+                    <?php $cont+=$solicitud->cantidad;$acum+=$solicitud->monto; ?>
+
+                    <?php $anterior = $solicitud->{$primera_columna[$i]}; ?>
+                @endforeach
+                <tfoot>
+                @if(count($columnas[$i])>1)
                     <tr>
                         <th class="titulo-tabla" colspan="{{count($columnas[$i])}}">Total {{ $solicitud->getValorReporte($primera_columna[$i]) }}</th>
                         <th class="titulo-tabla texto-derecha">{{$cont}}</th>
                         <th class="titulo-tabla texto-derecha">{{tm($acum)}}</th>
                     </tr>
-                    <?php $cont = 0; $acum=0; ?>
                 @endif
                 <tr>
-                    @foreach($columnas[$i] as $key=>$columna)
-                        <td style="width: {{80/count($columnas[$i])}}%;" class="fila-tabla">{{ $solicitud->getValorReporte($key) }}</td>
-                    @endforeach
-                    <td class="fila-tabla texto-derecha" style="width: 10%;">
-                        {{$solicitud->cantidad}}
-                    </td>
-                    <td class="fila-tabla texto-derecha" style="width: 10%;">
-                        {{tm($solicitud->monto)}}
-                    </td>
+                    <th class="titulo-tabla" colspan="{{count($columnas[$i])}}">Total</th>
+                    <th class="titulo-tabla texto-derecha">{{$solicitudes[$i]->sum('cantidad')}}</th>
+                    <th class="titulo-tabla texto-derecha">{{tm($solicitudes[$i]->sum('monto'))}}</th>
                 </tr>
-                <?php $cont+=$solicitud->cantidad;$acum+=$solicitud->monto; ?>
-
-                <?php $anterior = $solicitud->{$primera_columna[$i]}; ?>
-            @endforeach
-            <tfoot>
-            @if(count($columnas[$i])>1)
-                <tr>
-                    <th class="titulo-tabla" colspan="{{count($columnas[$i])}}">Total {{ $solicitud->getValorReporte($primera_columna[$i]) }}</th>
-                    <th class="titulo-tabla texto-derecha">{{$cont}}</th>
-                    <th class="titulo-tabla texto-derecha">{{tm($acum)}}</th>
-                </tr>
+                </tfoot>
             @endif
-            <tr>
-                <th class="titulo-tabla" colspan="{{count($columnas[$i])}}">Total</th>
-                <th class="titulo-tabla texto-derecha">{{$solicitudes[$i]->sum('cantidad')}}</th>
-                <th class="titulo-tabla texto-derecha">{{tm($solicitudes[$i]->sum('monto'))}}</th>
-            </tr>
-            </tfoot>
         </table>
         <?php $cont=0;$acum=0; ?>
         <br>
--- app/views/reportes/pdf/general.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('reportes.pdf.master')
-@section('reporte')
-   
-@endsection
