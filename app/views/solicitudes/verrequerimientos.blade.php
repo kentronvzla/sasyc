00ab3849e34b45d983823c2eb5b66c37ebeb7849
unknown <Edgar Landaeta@HP-MXJ7160B54.kentron.com.ve>
2015-03-17 17:13:41 -0430

    bandeja principal terminada

--- app/views/solicitudes/verrequerimientos.blade.php
+++ app/views/solicitudes/verrequerimientos.blade.php
@@ -18,7 +18,7 @@
             </div>
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">CerraR</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
         </div>
     </div>
 </div>
