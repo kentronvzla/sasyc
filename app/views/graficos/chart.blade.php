130b5f38602e397db550a259e65471eed2421de8
Edgar Landaeta <Edgar Landaeta@HP-MXJ7160B54.kentron.com.ve>
2015-04-08 16:52:47 -0430

    vista para probar graficos

---
+++ app/views/graficos/chart.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!--
+            ===
+            This comment should NOT be removed.
+    
+            Charisma v2.0.0
+    
+            Copyright 2012-2014 Muhammad Usman
+            Licensed under the Apache License v2.0
+            http://www.apache.org/licenses/LICENSE-2.0
+    
+            http://usman.it
+            http://twitter.com/halalit_usman
+            ===
+        -->
+        <meta charset="utf-8">
+        <title>Free HTML5 Bootstrap Admin Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+        <meta name="author" content="Muhammad Usman">
+
+        <!-- The styles -->
+        <link href='../css/morris.css' rel='stylesheet'>
+
+        <!-- jQuery -->
+        <script src="../bower_components/jquery/jquery.min.js"></script>
+        <script src="../js/morris.min.js"></script>
+        <script src="../js/morris.js"></script>
+        <script src="../js/raphael.min.js"></script>
+        <script src="../js/views/graficos.js"></script>
+
+        <!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- The fav icon -->
+        <link rel="shortcut icon" href="img/favicon.ico">
+    </head>
+
+    <body>
+        <!-- topbar starts -->
+        <div class="navbar navbar-default" role="navigation">
+            <div id="chartano" style="height: 250px;"></div>
+        </div>
+        <!-- topbar starts -->
+        <div class="navbar navbar-default" role="navigation">
+            <div id="chartarea" style="height: 250px;"></div>
+        </div> 
+
+        <div class="morris-hover morris-default-style" style="left: 0px; top: 173px;">
+            <div class="morris-hover-row-label"></div>
+            <div class="morris-hover-point" style="color: #0b62a4">
+            </div>
+            <div class="morris-hover-point" style="color: #7a92a3">
+            </div>                
+        </div>
+    </body>
+</html>
