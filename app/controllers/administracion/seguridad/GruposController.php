e13cdad50705b73fef1101a003eec09f1db1c36e
unknown <Dhaily Robles@HP-MXJ7160B54.kentron.com.ve>
2015-07-15 08:56:38 -0430

    Modificacion del controlador de grupos y creacion de una nueva vista para agregar un nuevo grupo de usuarios y poder asignar la permisologia

--- app/controllers/administracion/seguridad/GruposController.php
+++ app/controllers/administracion/seguridad/GruposController.php
@@ -5,7 +5,44 @@ class GruposController extends \Administracion\TablasBaseController {
         parent::__construct();
     }
     public function getModificar($id = null) {
+        if ($id==null) {
+            $data['grupo'] =  new \Grupo();
+              $permisosGlobales = \Grupo::$permisos;
 
+        try {
+            $sentryGroup = \Sentry::findAllGroups();
+        } catch (Exception $ex) {
+            $sentryGroup = null;
+        }
+
+        if (is_object($sentryGroup)) {
+            $data['permisos'] = array();
+
+            foreach ($permisosGlobales as $key => $permiso) {
+                $data['permisos'][$key] = array(
+                    'Descripcion' => array_values($permiso)[0],
+                );
+
+                $tiene = false;
+
+                foreach ($permiso as $per => $descripcion) {
+                    if ($sentryGroup->hasAccess($per)) {
+                        $data['permisos'][$key][$per] = $descripcion;
+                        $tiene = true;
+                    }
+                }
+                if (!$tiene) {
+                    unset($data['permisos'][$key]);
+                }
+            }
+        } else {
+            $data['permisos'] = array(
+                '' => array(),
+            );
+        }
+           return \View::make('administracion.seguridad.creargrupos', $data);
+            
+        }
         $data['grupo'] = \Grupo::find($id);
 
         $permisosGlobales = \Grupo::$permisos;
@@ -62,4 +99,13 @@ class GruposController extends \Administracion\TablasBaseController {
         return \Response::json(array('mensaje' => 'Se denego el permiso correctamente.'));
     }
 
+    public function crear(){
+        
+        echo 'paso';
+        
+        
+    }
+    
+    
+    
 }
