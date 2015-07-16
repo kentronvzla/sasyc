a84622288b173c80b22ff7eeebbe569b863c40ca
unknown <Dhaily Robles@HP-MXJ7160B54.kentron.com.ve>
2015-07-16 14:34:13 -0430

    modificacion de la vista de usuarios para quitar el boton de eliminar

--- app/views/administracion/seguridad/usuarios.blade.php
+++ app/views/administracion/seguridad/usuarios.blade.php
@@ -3,7 +3,7 @@
 <div class="panel panel-danger">
     <div class="panel-heading">Usuarios registrados en el sistema</div>
     <div class="panel-body">
-        {{HTML::tableModel($usuarios, 'Usuario')}}
+        {{HTML::tableModel($usuarios, 'Usuario',false)}}
     </div>
 </div>
 @stop
