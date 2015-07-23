b9173cc5fb9d51017730e882f09faf8cfb0cf7f6
unknown <Reysmer Valle@KNTDSW02W8.kentron.com.ve>
2015-07-23 11:21:19 -0430

    Modificacion ConfiguracionController para autenticacion en el webservice procesar.

--- app/controllers/ConfiguracionController.php
+++ app/controllers/ConfiguracionController.php
@@ -15,16 +15,18 @@ class ConfiguracionController extends BaseController {
     }
 
     public function getParametros() {
+        $userws = \Configuracion::get('usuariows');
+        $passws = \Configuracion::get('passws');
         if ((!isset($_SERVER['PHP_AUTH_USER'])) || (!isset($_SERVER['PHP_AUTH_PW']))) {
             header('WWW-Authenticate: Basic realm="Secured Area"');
             header('HTTP/1.0 401 Unauthorized');
             $response = ['error' => 'Authorization Required'];
         } else if ((isset($_SERVER['PHP_AUTH_USER'])) && (isset($_SERVER['PHP_AUTH_PW']))) {
-            if (($_SERVER['PHP_AUTH_USER'] != "5@5yc") || ($_SERVER['PHP_AUTH_PW'] != "5@5yc")) {
+            if (($_SERVER['PHP_AUTH_USER'] != $userws) || ($_SERVER['PHP_AUTH_PW'] != $passws)) {
                 header('WWW-Authenticate: Basic realm="Secured Area"');
                 header('HTTP/1.0 401 Unauthorized');
                 $response = ['error' => 'Authorization Required'];
-            } else if (($_SERVER['PHP_AUTH_USER'] == "5@5yc") || ($_SERVER['PHP_AUTH_PW'] == "5@5yc")) {
+            } else if (($_SERVER['PHP_AUTH_USER'] == $userws) || ($_SERVER['PHP_AUTH_PW'] == $passws)) {
                 $response = array(
                     'driver_pgsql' => Config::get('database.connections.pgsql.driver'),
                     'host_pgsql' => Config::get('database.connections.pgsql.host'),
