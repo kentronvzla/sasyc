4293aced03535d6e9015e54591a02971575a9e2b
unknown <Dhaily Robles@HP-MXJ7160B54.kentron.com.ve>
2015-08-03 12:15:10 -0430

    refresh de la pagina para asignar los permisos

--- app/views/administracion/seguridad/gruposform.blade.php
+++ app/views/administracion/seguridad/gruposform.blade.php
@@ -1,15 +1,14 @@
 <div class="modal-dialog modal-lg">
-    
     {{Form::open(array('url'=>'administracion/seguridad/grupo'))}}
     <div class="modal-content">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Cerrar</span></button>
-            <h4 class="modal-title" id="myModalLabel">Agregar/Modificar</h4>
+            <h4 class="modal-title" id="myModalLabel">Modificar Permisos</h4>
         </div>
         {{Form::concurrencia(@$grupo)}}
         {{Form::hidden('idgrupo',@$grupo->id)}}
         <div class="modal-body">
-            
+
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
@@ -29,14 +28,14 @@
                         @include('administracion.seguridad.tablapermisos',array('permisos'=>$permisos,'asignados'=>true))
                     </div> 
                 </div>
-            </div>
-         
+            </div>     
             @endif
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-            <button type="submit" class="btn btn-primary" data-dismiss="modal">Guardar</button>
+        <div class="modal-footer">   
+            <a onclick="document.location.reload('administracion/seguridad/grupo')" class="btn btn-default" role="button" type="button">Cancelar</a>  
+            <a onclick="document.location.reload('administracion/seguridad/grupos')" class="btn btn-primary" role="button" type="button">Guardar</a>  
         </div>
     </div>
     {{Form::close()}}
+
 </div>
